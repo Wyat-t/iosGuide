1d2fdf4b7dfb883349429c9f58574dd97a687b31
wyatt <Wyat_t@126.com>
2019-09-24 16:39:00 +0800

    1

--- Guide/ViewController.swift
+++ Guide/ViewController.swift
@@ -13,6 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //let initView = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+        let initView = UIScrollView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+        initView.backgroundColor = UIColor.white
+        self.view = initView
+        let secondView = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+        secondView.backgroundColor = UIColor.blue
+        //self.view.addSubview(secondView)
+        initView.addSubview(secondView)
+        UIView.animate(withDuration: 1){
+            var trans = secondView.transform
+            trans = trans.translatedBy(x: 100, y: 700)
+            secondView.transform = trans
+        }
     }
 
 
