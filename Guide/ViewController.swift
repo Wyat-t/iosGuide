55764bb4fd32544a6c229675ac1e94c2151a77d7
wyatt <Wyat_t@126.com>
2019-10-07 20:08:39 -0700

    1008

--- Guide/ViewController.swift
+++ Guide/ViewController.swift
@@ -13,15 +13,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        phone.delegate = self as? UITextFieldDelegate
     }
     @IBOutlet weak var phone: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
     
     @IBAction func phoneAction(_ sender: UITextField, forEvent event: UIEvent) {
         if(phone.text=="123") {phone.text="456"}
         phone.clearButtonMode = UITextField.ViewMode.always
-        
     }
     
+    @IBAction func confirmTouchUp(_ sender: UIButton) {
+        nameLabel.text = phone.text
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //phone.resignFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //phone.endEditing(true)
+    }
     
 }
 
